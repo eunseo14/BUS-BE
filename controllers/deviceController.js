4f40cf5c14310ae9cdeb7ea2753299febb3a786f
malcha54 <hjhj0214@naver.com>
2025-08-16 14:49:22 +0900

    fix: 변수명 오타 수정 lon->lng

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,27 +1,29 @@
 const { getDistanceFromLatLonInMeters } = require('../services/mathService');
 
 //강남역 근처 좌표
-const destination = { lat: 37.4979, lon: 127.0276 };
+const destination = { lat: 37.4979, lng: 127.0276 };
 
-let latestLocation = null;
+let latestLocation = { lat: 37.4977, lng: 127.0275 };
 
 const postLocation = (req, res) => {
-  const { lat, lon } = req.body;
-  if (!lat || !lon) return res.status(400).json({ error: "lat/lon required" });
+  const { lat, lng } = req.body;
+  if (!lat || !lng) return res.status(400).json({ error: "lat/lng required" });
 
-  latestLocation = { lat, lon };
+  latestLocation = { lat, lng };
   res.json({ message: "Location received!" });
+  console.log(latestLocation);
 };
 
 const getLedStatus = (req, res) => {
   if (!latestLocation) return res.json({ led: false });
 
   const distance = getDistanceFromLatLonInMeters(
-    latestLocation.lat, latestLocation.lon,
-    destination.lat, destination.lon
+    latestLocation.lat, latestLocation.lng,
+    destination.lat, destination.lng
   );
   console.log(distance);
   const isNear = distance <= 100;
+  console.log(isNear);
   res.json({ led: isNear });
 };
 
